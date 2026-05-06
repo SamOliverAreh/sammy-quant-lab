f33a9acac65786a28fb33230c804bcc79b0635f2
Sammy <164457887+SamOliverAreh@users.noreply.github.com>
2026-05-06 14:45:27 +0800

    Add type hints to preprocess function

--- data_pipeline/preprocessing.py
+++ data_pipeline/preprocessing.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def preprocess(df):
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
-    df["returns"] = df["Close"].pct_change()
+    df["returns"]     = df["Close"].pct_change()
     df["log_returns"] = np.log(df["Close"] / df["Close"].shift(1))
     df.dropna(inplace=True)
     return df
