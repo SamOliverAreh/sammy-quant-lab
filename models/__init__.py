0cea7efa07632b19b48eec37071a9d846be656d5
Sammy <164457887+SamOliverAreh@users.noreply.github.com>
2026-05-06 14:56:16 +0800

    Enhance model imports in __init__.py
    Added new imports for various forecasting models and updated __all__ to include them.

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,16 @@
 from .statistical.arima import arima_forecast
-from .statistical.garch import garch_volatility
-from .ml.lstm import train_lstm, forecast_lstm
-from .hybrid.arima_lstm import hybrid_forecast
+from .statistical.garch import garch_volatility, garch_summary
+from .statistical.ets   import ets_forecast
+from .statistical.prophet_model import prophet_forecast
 
-__all__ = [
-    "arima_forecast",
-    "garch_volatility",
-    "train_lstm",
-    "forecast_lstm",
-    "hybrid_forecast",
-]
+from .ml.lstm             import train_lstm, forecast_lstm
+from .ml.gru              import train_gru, forecast_gru
+from .ml.transformer_model import train_transformer, forecast_transformer
+from .ml.xgboost_model    import train_xgboost, forecast_xgboost
+from .ml.random_forest    import train_rf, forecast_rf
+
+from .hybrid.arima_lstm   import hybrid_forecast
+from .hybrid.ets_gru      import ets_gru_forecast
+from .hybrid.prophet_xgb  import prophet_xgb_forecast
+
+from .ensemble.stacked    import stacked_ensemble_forecast
